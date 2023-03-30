988b859db3ca5c51597713fa905cc27fe8863a37
Danila <golubev.d.a@mail.ru>
2023-03-30 10:07:32 +0300

    fix reflection

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,7 @@ const empty = '';
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   usersPath: () => [apiPath, 'data'].join('/'),
-  signUpPath: () => [apiPath, '/signup'].join('/'),
+  signUpPath: () => [apiPath, 'signup'].join('/'),
   notFoundPath: () => [empty, '*'].join('/'),
   loginPagePath: () => [empty, 'login'].join('/'),
   signUpPagePath: () => [empty, 'signup'].join('/'),
